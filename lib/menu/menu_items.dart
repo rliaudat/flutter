a5f934169c1857a530f2d30e3003378b151143d0
Rodrigo <rliaudat@gmail.com>
2023-06-08 18:41:37 -0300

    Estilos de la app

---
+++ lib/menu/menu_items.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final String link;
+
+  const MenuItem({
+    required this.title, 
+    required this.subtitle, 
+    required this.icon, 
+    required this.link});
+}
+
+const appMenuItems = <MenuItem> [
+  
+  MenuItem (
+    title: 'Botones',
+    subtitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button
+  ),
+
+  MenuItem (
+    title: 'Tarjetas',
+    subtitle: 'Contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card
+  ),
+
+];
