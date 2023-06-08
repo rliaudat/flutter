fca9e44c85857e49021fa0eb21b74753efc9b6da
Rodrigo <rliaudat@gmail.com>
2023-06-08 19:08:10 -0300

    Home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_app/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,50 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text ('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView(); // La key no va xq es privada y solo se llama desde aca
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuItems;
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+        }, 
     );
   }
 }
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon ( menuItem.icon, color: colors.primary),
+      trailing: Icon (Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text (menuItem.title),
+      subtitle: Text (menuItem.subtitle),
+      onTap: () {
+
+      },
+      );
+  }
+}
+
