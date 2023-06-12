977ef31e907c38c49edc4a90cc85d33f91e9ae5e
Rodrigo <rliaudat@gmail.com>
2023-06-12 06:34:57 -0300

    Buttons

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_app/menu/menu_items.dart';
+import 'package:flutter_widget_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -51,7 +52,11 @@ class _CustomListTile extends StatelessWidget {
       title: Text (menuItem.title),
       subtitle: Text (menuItem.subtitle),
       onTap: () {
-
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen (),
+            ),
+        );
       },
       );
   }
